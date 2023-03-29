79766fb7ccae84779e232cc8b64a7ccd8c149362
rokers <chinakarishev21@gmail.com>
2023-03-29 14:50:01 +0400

    challenge: first

---
+++ Assets/ExitGame.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    public void Exit()
+    {
+        Debug.Log("Exiting the game");
+        return;
+    }
+}
